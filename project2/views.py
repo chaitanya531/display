ba1d4c2ebdf1e541a75711e346dabfe8a5068167
S20180020214 <krishnachaitanya.j18@iiits.in>
2019-11-04 21:12:13 +0530

    commit2

--- project2/views.py
+++ project2/views.py
@@ -6,9 +6,9 @@ def display1(request):
     text1=request.GET['text1']
     text2=request.GET['text2']
     if text1=='sai':
-        text3='sai your details are'
+        text3='sai your details are :'
         return render(request,'display.html',{'text1':text1,'text2':text2,'text3':text3})
     else:
-         text3='raj your details are'
-         return render(request,'display2.html',{'text1':text1,'text2':text2,'text3':text3})
+         text4='Student  details are :'
+         return render(request,'display2.html',{'text1':text1,'text2':text2,'text3':text4})
 
